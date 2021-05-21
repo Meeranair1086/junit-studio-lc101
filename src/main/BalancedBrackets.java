ba755859044646d54ab7bac086323f2f11023cdd
Meera Nair <murali.meera@gmail.com>
2021-05-20 21:28:36 -0500

    Initial commit

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,35 +1,79 @@
 package main;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BalancedBrackets {
+    public static void main(String[] args) {
+
+        System.out.println(BalancedBrackets.hasBalancedBrackets("{LaunchCode}"));
+        System.out.println(BalancedBrackets.hasBalBrackets("{LaunchCode}"));
+        System.out.println(BalancedBrackets.hasTwoBackBrackets("]["));
+    }
+
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
+
         int brackets = 0;
         for (char ch : str.toCharArray()) {
-            if (ch == '[') {
+            if (ch == '[' ) {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+
+            }
+        }
+        return brackets == 0;
+    }
+    public static boolean hasBalBrackets(String str) {
+        boolean balanced = false;
+        int brackets = 0;
+        for (char ch : str.toCharArray()) {
+            if ((ch == '[') || (ch == ']')) {
+                System.out.println("Inside if");
+                brackets++;
+                balanced = true;
+            } else if (ch == ']') {
+                System.out.println("Inside else if 1");
+                brackets--;
+                balanced = false;
+            }
+        }
+        return balanced;
+    }
+
+    public static boolean hasTwoBackBrackets(String str){
+        boolean balance = false;
+        int brackets = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == ']' ) {
+                brackets--;
+            } else if (ch == '[') {
+                brackets--;
+
             }
         }
         return brackets == 0;
     }
 }
+
+
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,69 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
+
 public class BalancedBracketsTest {
+
+
 
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyOneBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyNonBracketReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void onlyEmptyReturnFalse(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyLettersBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void onlyLettersOneBracketsReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void onlyTwoBackBrackets(){
+        assertFalse(BalancedBrackets.hasTwoBackBrackets("]["));
+    }
+    @Test
+    public void onlyTwoBackWithLettersBrackets(){
+        assertFalse(BalancedBrackets.hasTwoBackBrackets("Launch]Code["));
+    }
+    @Test
+    public void hasCurlyBracketsInsteadOfSquareBracket(){
+        assertFalse(BalancedBrackets.hasBalBrackets("{LaunchCode}"));
+    }
+    @Test
+    public void hasBracketsInsteadOfSquareBracket(){
+        assertFalse(BalancedBrackets.hasBalBrackets("(LaunchCode)"));
+    }
+    @Test
+    public void hasSpaceInsteadOfSquareBracket(){
+        assertFalse(BalancedBrackets.hasBalBrackets("Launch Code"));
+    }
+
 
 
 }
